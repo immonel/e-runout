f55cb15ba1c8202e68871b6266acf3e42f0e75a3
Leo Immonen <leo.immonen@aalto.fi>
2022-08-16 16:58:47 +0300

    Updated mongoose schema

--- server/models/measurement.js
+++ server/models/measurement.js
@@ -1,14 +1,37 @@
 const mongoose = require('mongoose')
 
 const measurementSchema = mongoose.Schema({
+  id: { type: String, required: true },
   name: { type: String, required: true },
-  data: [{
-    timestamp: { type: Date, required: true },
-    values: [{
-      name: { type: String, required: true },
-      value: { type: Number, required: true }
-    }]
+  type: {
+    type: String,
+    enum: ['measurement', 'calibration'],
+    required: true
+  },
+  created: { type: String, required: true },
+  componentRef: { type: String },
+  regressionCoefficient: { type: Number },
+  scaleFactor: { type: Number },
+  datasets: [{
+    name: { type: String, required: true },
+    type: {
+      type: String,
+      enum: ['ttl', 'eddy', 'erunout'],
+      required: true
+    },
+    data: [ Number ]
   }]
+},
+{
+  collection: 'measurements'
+})
+
+measurementSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 const Measurement = mongoose.model('Measurement', measurementSchema)
