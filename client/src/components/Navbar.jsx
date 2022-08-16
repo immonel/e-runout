0142a182e01e935014e0412c44e0b207fdf9a655
Leo Immonen <leo.immonen@aalto.fi>
2022-08-16 19:36:14 +0300

    Added info text for dev environment

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,6 +5,10 @@ const _Navbar = () => (
   <Navbar bg="light" expand="lg">
     <Container className='navbar-content'>
       <Navbar.Brand>Electrical runout analyzer</Navbar.Brand>
+        {
+          process.env.NODE_ENV === 'development' &&
+            <p className='my-0 text-secondary'>Development environment</p>
+        }
         <Image width='150px' src='aalto-logo.png' fluid />
     </Container>
   </Navbar>
