0fba7af63d64a7770cd93ab6c70eb7f4e72a4a08
Leo Immonen <leo.immonen@aalto.fi>
2022-08-17 17:10:46 +0300

    Added print button for measurement modal

--- client/src/components/Measure/MeasurementModal.jsx
+++ client/src/components/Measure/MeasurementModal.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useRef } from 'react'
 import { Button, Card, Modal } from 'react-bootstrap'
+import { BsPrinter } from 'react-icons/bs';
+import ReactToPrint from 'react-to-print';
 import MeasurementChart from './MeasurementChart'
 
+const PrintWrapper = React.forwardRef((props, ref) => (
+  <div className='print-wrapper' ref={ref}>
+    {props.children}
+  </div>
+))
+
 const MeasurementModal = ({ measurement, show, setShow }) => {
+  const componentRef = useRef()
 
   const measurementInfo = JSON.stringify({
     ...measurement,
@@ -14,19 +24,30 @@ const MeasurementModal = ({ measurement, show, setShow }) => {
 
   return (
     <Modal size='xl' centered show={show} onHide={() => setShow(false)}>
-      <Modal.Header>
-        <h3>Measurement {measurement.name}</h3>
-        <Button variant='outline-secondary' onClick={() => setShow(false)}>⨉</Button>
-      </Modal.Header>
-      <Modal.Body>
-        <MeasurementChart measurement={measurement}  />
-      </Modal.Body>
-      <Card>
-        <Card.Body>
-          <h5>Measurement info:</h5>
-          <pre>{measurementInfo}</pre>
-        </Card.Body>
-      </Card>
+      <PrintWrapper ref={componentRef}>
+        <Modal.Header>
+          <h3>Measurement {measurement.name}</h3>
+          <div>
+            <ReactToPrint
+              trigger={() =>
+                <Button className='mx-2' variant='outline-secondary'>
+                  <BsPrinter />
+                </Button>}
+              content={() => componentRef.current}
+            />
+            <Button variant='outline-secondary' onClick={() => setShow(false)}>⨉</Button>
+          </div>
+        </Modal.Header>
+        <Modal.Body>
+          <MeasurementChart measurement={measurement}  />
+        </Modal.Body>
+        <Card>
+          <Card.Body>
+            <h5>Measurement info:</h5>
+            <pre>{measurementInfo}</pre>
+          </Card.Body>
+        </Card>
+      </PrintWrapper>
     </Modal>
   )
 }
